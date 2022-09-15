fe865840953e0531921753b27b911701ec310b2c
Sachin kumar <95627047+Sachin7368@users.noreply.github.com>
2022-09-15 12:51:57 +0530

    main

--- src/index.js
+++ src/index.js
@@ -3,7 +3,16 @@ const fs = require('fs/promises')
 const myFileWriter = async (fileName, fileContent) => {
 	// write code here
 	// dont chnage function name
+	// fs.mkdir('node',function(){
+	// 	fs.writeFileSync('./node/File.txt',"Hello");
+	// });
+	fs.writeFile('File.txt',"Hello",(err) => {
+		if(err) throw err;
+		console.log("File has been saved!");
+	});
 }
+myFileWriter();
+
 
 const myFileReader = async (fileName) => {
 	// write code here
